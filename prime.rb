d8d5cd143fd5db063e45f9cc429104d38bb12b16
Miné E <mine_esen@yahoo.ca>
2019-05-30 23:18:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,12 +3,11 @@ def prime?(n)
   if n <= 1
     false
   end
-  for i in (2...n) do
+  for i in (2..n-1) do
     if n % i == 0
       false
     else
       true
     end
   end
-  binding.pry
 end

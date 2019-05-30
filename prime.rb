478c1e34307e7f1da78ad2ada72a7f7d36818408
Miné E <mine_esen@yahoo.ca>
2019-05-30 23:52:06 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,16 @@
+=begin
 def prime?(n)
   (2..(n-1)).each do |i|
     false if n % i == 0
+  end
+  true
+end
+=end
+def prime?(num)
+  n = 2
+  while n < num
+    false if num % n == 0
+    n += 1    
   end
   true
 end

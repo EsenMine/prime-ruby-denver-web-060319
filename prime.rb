84b314f3e645f9876b58c8469642263bd190edc3
Miné E <mine_esen@yahoo.ca>
2019-05-30 22:24:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,18 @@
-def prime?(number)
-  for i in 2..(number-1)
-    if n % i == 0
-      false
-    else
-      true
-    end
+def prime?(n)
+  if n <= 3
+    return n > 1
+  elsif n % 2 == 0 || n % 3 == 0
+    return false
+  i = 5
+  while i * i <= n
+    if n % i == 0 || n % (i+2) == 0
+      return false
+    i = i + 6
+  return true
+  end
+
+  end
+
+
   end
 end

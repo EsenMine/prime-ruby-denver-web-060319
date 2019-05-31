f5d0f125bc4b2dcd7e3cb202a3cc83552b060662
Miné E <mine_esen@yahoo.ca>
2019-05-31 00:27:26 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,4 @@
 def prime?(n)
-  for i in (2..(n-1)) do
-    if n % i == 0
-      false
-    else
-      true
-    end
-  end
-end
-=begin
-def prime?(n)
   (2..(n-1)).each do |i|
     false if n % i == 0 || n < 0
   end
@@ -27,5 +17,17 @@ def prime?(num)
     n += 1
   end
   true
+end
+=end
+
+=begin this is failing on both
+def prime?(n)
+  for i in (2..(n-1)) do
+    if n % i == 0
+      false
+    else
+      true
+    end
+  end
 end
 =end
